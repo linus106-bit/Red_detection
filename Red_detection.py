7731132751129da9a8cc0ef7d47b342aa7b3a43a
linus106-bit <linus3390@gmail.com>
2020-08-31 13:05:41 +0900

    20200831

--- Red_detection.py
+++ Red_detection.py
@@ -6,7 +6,7 @@ import math
 cap = cv2.VideoCapture(0)
 
 class FindNonZero:
-    def AllNonZero(height,width,img): #빨간색의 물체 크기(총 개수) 찾는 함수
+    def FindSize(height,width,img): #빨간색의 물체 크기(총 개수) 찾는 함수
         count_img = 0
         x_axis = np.array([])
         y_axis = np.array([])
@@ -14,31 +14,30 @@ class FindNonZero:
             for j in range(height):
                 if img[int(j),int(i)] > 0:
                     count_img += 1
-                    x_axis = np.append(x_axis,i)
-                    y_axis = np.append(y_axis,j)
                 else:
                     pass
-        return count_img, x_axis, y_axis
+        return count_img
 
-
-
-class Findcenter:
-    def wherecenter(height,width,img): #빨간색물체의 중심을 찾는 함수 
+    def wherecenter(height,width,img): #빨간색 물체의 중심을 찾는 함수 
         height_img = np.array([])
         width_img = np.array([])
         bottom_img = np.array([])
-        for i in range(height):
+        for i in range(height): #y좌표 찾기
             for j in range(width):
                 if img[int(i),int(j)]>0:
                     height_img = np.append(height_img, i)
                     width_img = np.append(width_img, j)
                 else:
                     pass
-        for i in range(height_img[0],height):
-            if img[int(i),width_img[0]]>0:
+
+        for i in range(int(height_img[0]),int(height)): #x좌표 찾기
+            if img[int(i),int(width_img[0])] > 0:
                 pass
             else:
                 bottom_img = np.append(bottom_img, i)
+
+        center_y=int((height_img[0]+bottom_img[0])/2)
+        center_x=int(width_img[0])
         print("center's height: ",int((height_img[0]+bottom_img[0])/2))
         print("center's width: ",width_img[0])
         if width_img[0]>(width/2):
@@ -47,7 +46,7 @@ class Findcenter:
             print("go right")
         if width_img[0]==(width/2):
             print("center")
-
+        return center_x, center_y
 
 
 while(cap.isOpened()):
@@ -61,17 +60,16 @@ while(cap.isOpened()):
         height, width = frame.shape[:2] # 높이 너비
         
         # 빨간색은 hsv에서 0-10, 170-180 두개로 검출해야됨
-        lower_red = cv2.inRange(hsv, (0,100,100), (5,255,255))
-        upper_red = cv2.inRange(hsv, (175,100,100), (180,255,255))
+        lower_red = cv2.inRange(hsv, (0,100,100), (3,255,255))
+        upper_red = cv2.inRange(hsv, (178,100,100), (180,255,255))
         added_red = cv2.addWeighted(lower_red, 1.0 ,upper_red, 1.0, 0.0) # 합친거
         red = cv2.bitwise_and(frame, frame, mask= added_red) #마스크를 씌움
         h,s,v = cv2.split(red)  #채널 개수를 1개로 만듬
-        size_img, x_axis, y_axis = FindNonZero.AllNonZero(height,width,v)
-        if size_img > 6000:
-            print("size: ", size_img)
-            print("x_axis: \n", x_axis)
-            print("y_axis: \n", y_axis)
-        cv2.imshow("new_red",v)
+        center_x, center_y = FindNonZero.wherecenter(height, width, v)
+        size = FindNonZero.FindSize(height, width, v)
+        print("Size: ", size)
+        red = cv2.line(red,(center_x,center_y),(center_x,center_y),(0,255,0),5)
+        cv2.imshow("new_red",red)
         k = cv2.waitKey(1) & 0xFF
         if k == 27 :
             break
