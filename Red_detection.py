ca98138423048383e1af9c255d09c854f63b4e66
linus106-bit <linus3390@gmail.com>
2020-09-04 15:14:51 +0900

    20200904

--- Red_detection.py
+++ Red_detection.py
@@ -14,15 +14,14 @@ class FindNonZero:
 
         center_x, center_y, length, count_img = 0,0,0,0
 
-        for i in range(height): #y좌표 찾기
+        for i in range(height): #y 좌표 찾기
             for j in range(width):
-                if img[int(i),int(j)]>0:
+                if img[int(i),int(j)] >0:
                     first_height_img = np.append(first_height_img, i)
                     first_width_img = np.append(first_width_img, j)
                     count_img += 1
-                else:
-                    pass
-            if len(first_width_img) < 25:  #무시하는 코드 새로 추가
+        
+            if len(first_width_img) < 25:  # 크기가 작은 노이즈는 무시하는 코드 
                 first_width_img = np.array([])
                 first_height_img = np.array([])
             else:
@@ -32,15 +31,16 @@ class FindNonZero:
 
         try:
             if len(height_img) > 0:
-                for i in range(int(height_img[0]),int(height)): #x좌표 찾기
+                for i in range(int(height_img[0]),int(height)): #x 좌표 찾기
                     if img[int(i),int(width_img[0])] > 0:
                         pass
                     else:
                         bottom_img = np.append(bottom_img, i)
-                print("height: ",height_img)
-                print("width: ",width_img)
+                        break
+                
+                print("height:",height_img)
                 print("bottom: ",bottom_img)
-
+                print("width: ",width_img)
 
                 center_y= int((height_img[0]+bottom_img[0])/2)
                 center_x= int(width_img[0])
@@ -72,19 +72,20 @@ while(cap.isOpened()):
         height, width = frame.shape[:2] # 높이 너비
         
         # 빨간색은 hsv에서 0-10, 170-180 두개로 검출해야됨
-        lower_red = cv2.inRange(hsv, (0,100,100), (5,255,255))
-        upper_red = cv2.inRange(hsv, (175,100,100), (180,255,255))
-        added_red = cv2.addWeighted(lower_red, 1.0 ,upper_red, 1.0, 0.0) # 합친거
-        red = cv2.bitwise_and(frame, frame, mask= added_red) #마스크를 씌움
+        lower_red = cv2.inRange(hsv, (110, 100, 100), (130, 255, 255))
+        #upper_red = cv2.inRange(hsv, (175,100,100), (180,255,255))
+        #added_red = cv2.addWeighted(lower_red, 1.0 ,upper_red, 1.0, 0.0) # 합친거
+        red = cv2.bitwise_and(frame, frame, mask= lower_red) #마스크를 씌움
         h,s,v = cv2.split(red)  #채널 개수를 1개로 만듬
 
-        center_x, center_y, length, size= FindNonZero.wherecenter(height, width, v)
+        center_x, center_y, length, size= FindNonZero.wherecenter(height, width, h)
+        print("size:",size)
         if size > 100:
             red = cv2.line(red,(center_x,center_y),(center_x,center_y),(0,255,0),3)
             red = cv2.rectangle(red, (center_x-length,center_y-length), (center_x+length,center_y+length), (0,255,0), 3)
         else:
             pass
-
+        cv2.imshow("v",h)
         cv2.imshow("new_red",red)
         k = cv2.waitKey(1) & 0xFF
         if k == 27 :
@@ -92,3 +93,4 @@ while(cap.isOpened()):
           
 cap.release()
 cv2.destroyAllWindows()
+
